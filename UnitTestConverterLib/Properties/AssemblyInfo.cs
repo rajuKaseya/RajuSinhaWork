2457db105f070b6212a99ee2b1ea4473051acf4a
raju sinha <raju.sinha@hotmail.com>
2020-05-10 00:20:12 +0530

    Added Unit Test

---
+++ UnitTestConverterLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestConverterLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestConverterLib")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("290d544f-6124-4c17-832b-c90ad022fc07")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestConverterLib/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestConverterLib
+{
+    [TestClass]
+    public class ConverterTests
+    {
+        [TestMethod]
+        public void TestConvert()
+        {
+            string Actual = "";
+            string expected = ",";
+            Assert.AreEqual(expected,Actual);
+        }
+    }
+}
