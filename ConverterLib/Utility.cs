fe2c617a417237e7d73bb306e3f9e8a24fcd0f83
raju sinha <raju.sinha@hotmail.com>
2020-05-10 00:45:39 +0530

    GetWordFromNumber member implemented

--- ConverterLib/Utility.cs
+++ ConverterLib/Utility.cs
@@ -4,12 +4,70 @@ using System.Text;
 
 namespace ConverterLib
 {
-   public class Utility
+   internal class Utility
     {
 
-        public string getvalue()
+        internal static string GetWordFromNumber(int number)
         {
-            return string.Empty;
+            switch (number)
+            {
+                case 1:
+                    return "One";
+                case 2:
+                    return "Two";
+                case 3:
+                    return "Three";
+                case 4:
+                    return "Four";
+                case 5:
+                    return "Five";
+                case 6:
+                    return "Six";
+                case 7:
+                    return "Seven";
+                case 8:
+                    return "Eight";
+                case 9:
+                    return "Nine";
+                case 10:
+                    return "Ten";
+                case 11:
+                    return "Eleven";
+                case 12:
+                    return "Twelve";
+                case 13:
+                    return "Thirteen";
+                case 14:
+                    return "Fourteen";
+                case 15:
+                    return "Fifteen";
+                case 16:
+                    return "Sixteen";
+                case 17:
+                    return "Seventeen";
+                case 18:
+                    return "Eighteen";
+                case 19:
+                    return "Nineteen";
+                case 20:
+                    return "Twenty";
+                case 30:
+                    return "Thirty";
+                case 40:
+                    return "Fourty";
+                case 50:
+                    return "Fifty";
+                case 60:
+                    return "Sixty";
+                case 70:
+                    return "Seventy";
+                case 80:
+                    return "Eighty";
+                case 90:
+                    return "Ninty";
+                default:
+                    return "";
+            }
         }
 
     }
