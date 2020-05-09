2886e467740d351d0e79f46503ae3c2a6714ecdb
raju sinha <raju.sinha@hotmail.com>
2020-05-10 01:05:08 +0530

    Implemented Convert method

--- ConverterLib/ConcreteConverter.cs
+++ ConverterLib/ConcreteConverter.cs
@@ -6,7 +6,46 @@ namespace ConverterLib
     {
         public string Convert(int number)
         {
-            return "Not Implemented";
+            string result = string.Empty;
+            long temp = number; ;
+            if (temp > 9999999)
+            {
+                result = result + Utility.ParseNumber(System.Convert.ToInt32(temp / 10000000)) + " Crore,";
+                temp = temp - (temp / 10000000) * 10000000;
+            }
+            if (temp > 99999)
+            {
+                result = result + Utility.ParseNumber(System.Convert.ToInt32(temp / 100000)) + " Lac,";
+                temp = temp - (temp / 100000) * 100000;
+            }
+            if (temp > 999)
+            {
+                result = result + Utility.ParseNumber(System.Convert.ToInt32(temp / 1000)) + " Thousands,";
+                temp = temp - (temp / 1000) * 1000;
+            }
+            if (temp > 99)
+            {
+                result = result + Utility.ParseNumber(System.Convert.ToInt32(temp / 100)) + " Hundreds,";
+                temp = temp - (temp / 100) * 100;
+            }
+            if (temp > 0)
+            {
+                if (result.Length > 0 && result[result.Length - 1] == ',')
+                {
+                    result = result.Remove(result.Length - 1);
+                }
+                result = result + " and " + Utility.ParseNumber(System.Convert.ToInt32(temp));
+
+            }
+            if (result.Length > 0 && result.StartsWith(" and"))
+            {
+                result = result.Remove(0, 4);
+            }
+            if (result.Length > 0 && result.EndsWith(","))
+            {
+                result = result.Remove(result.Length - 1);
+            }
+            return result;
         }
     }
 }
